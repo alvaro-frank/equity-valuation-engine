cc70bcd9e65b3345cd9439a6a95b91cd2ddc4399
alvaro.franco <alvarojf96@hotmail.com>
2026-02-17 19:42:37 +0000

    feat: add QualitativeValuation Service

---
+++ src/services/qualitative_valuation_service.py
@@ -0,0 +1,34 @@
+from infrastructure.gemini_adapter import GeminiAdapter
+from services.dtos import QualitativeValuationDTO, TickerDTO
+
+class QualitativeValuationService:
+    """
+    Service responsible for performing stock qualitative valuation analysis based on the provided stock data.
+    This service takes in a TickerDTO, analyzes the quality, moat and background of a business, and returns a QualitativeValuationDTO containing the analysis results.
+    """
+    def __init__(self):
+        """
+        Initializes the QualitativeValuationService with the GeminiAdapter for AI-driven analysis.
+        """
+        self.gemini_adapter = GeminiAdapter()
+
+    def analyze_business(self, ticker_dto: TickerDTO) -> QualitativeValuationDTO:
+        """
+        Analyzes the qualitative aspects of a business, such as its history and business model, using AI analysis.
+        
+        Args:
+            ticker_dto (TickerDTO): The data transfer object containing the ticker's metadata.
+            
+        Returns:
+            QualitativeValuationDTO: The result of the qualitative analysis, including history and business description.
+        """
+        analysis_dict = self.gemini_adapter.analyze_company(
+            ticker=ticker_dto.symbol
+        )
+        
+        analysis_dict.pop("ticker", None)
+        
+        return QualitativeValuationDTO(
+            ticker=ticker_dto,
+            **analysis_dict
+        )
