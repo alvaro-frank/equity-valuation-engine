55a09b5e88134d29b8c4aeaf812f156ec35cbef6
alvaro.franco <alvarojf96@hotmail.com>
2026-02-17 19:44:08 +0000

    refactor: update main.py to perform both qualitative and quantitative analysis

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
-import sys
-from controllers.quantitative_valuation_controller import ValuationController
+from controllers.quantitative_valuation_controller import QuantitativeValuationController
+from controllers.qualitative_valuation_controller import QualitativeValuationController
+
+def main():
+    ticker = "MSFT"
+    
+    # 1. Quantitative Analysis
+    quant_controller = QuantitativeValuationController()
+    quant_controller.run(ticker, years=10)
+    
+    # Qualitative Analysis
+    qual_controller = QualitativeValuationController()
+    qual_controller.run(ticker)
 
 if __name__ == "__main__":
-    ticker = sys.argv[1] if len(sys.argv) > 1 else "MSFT"
-    
-    controller = ValuationController()
-    controller.run(ticker, years=10)
+    main()
